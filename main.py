0d75437a1c68195b63ad61e8c10fbb5fe5a1a58d
Earl Hikky <earl.hikky91@gmail.com>
2023-10-30 12:55:49 +0300

    second commit

--- main.py
+++ main.py
@@ -4,24 +4,21 @@ import gnupg
 import pyperclip
 
 
-# with open('/Users/earl/Library/Mobile Documents/com~apple~CloudDocs/my_private_key.asc') as f:
-#     key_data = f.read()
-# import_result = gpg.import_keys(key_data)
-
-
 def main():
-    gpg = gnupg.GPG(gnupghome='')
-    # gpg.encoding = 'utf-8'
+    gpg = gnupg.GPG()
     parser = argparse.ArgumentParser(description='gpg encrypt')
-    parser.add_argument('file', help='encrypted file')
-    path_file = parser.parse_args().file
-    # decrypt file
-    with open(path_file, 'rb') as enc_file:
-        dec_data = gpg.decrypt_file(enc_file)
-    if not dec_data.ok:
-        print(dec_data.problems)
-    pyperclip.copy(dec_data.data.strip().decode('UTF-8'))
-    print(dec_data.status)
+    parser.add_argument('filename', help='encrypted filename')
+    filename = parser.parse_args().filename
+    path_file = f'/Users/earl/.password-store/{filename}.gpg'
+    try:
+        with open(path_file, 'rb') as enc_file:
+            dec_data = gpg.decrypt_file(enc_file)
+        if not dec_data.ok:
+            print(dec_data.problems)
+        pyperclip.copy(dec_data.data.strip().decode('UTF-8'))
+        print(dec_data.status)
+    except FileNotFoundError:
+        print('wrong filename or file doesn\'t exists:', path_file)
 
 
 if __name__ == '__main__':
